1422a2fbf6824b4fbbfde6134ee80f698e14c116
Art Frich <frich.g23@yandex.ru>
2023-08-06 17:36:05 +0500

    fix: fucking cors setting

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,16 +2,18 @@ const { DEFAULT_ALLOWED_METHODS, allowedCors } = require('../helpers/constants')
 
 // eslint-disable-next-line consistent-return
 module.exports.cors = (req, res, next) => {
+  const requestHeaders = req.headers['access-control-request-headers'];
   const { origin } = req.headers;
   const { method } = req;
 
   // if (allowedCors.includes(origin)) {
   if (true) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   if (method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
 
