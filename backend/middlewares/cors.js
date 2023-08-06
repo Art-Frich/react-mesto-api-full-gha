009ec35da19dbfeb7639af3f35cf82942beb4908
Art Frich <frich.g23@yandex.ru>
2023-08-06 17:30:54 +0500

    fix: fucking cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -7,7 +7,7 @@ module.exports.cors = (req, res, next) => {
 
   // if (allowedCors.includes(origin)) {
   if (true) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Origin', '*');
   }
 
   if (method === 'OPTIONS') {
